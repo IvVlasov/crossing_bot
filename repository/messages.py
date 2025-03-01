30b58da4ca5ed6f0214d0f29b399d014bfc97c84
Ivan Vlasov <ivan.vlasov@raftds.com>
2025-03-01 23:50:14 +0300

    add create base messages

--- repository/messages.py
+++ repository/messages.py
@@ -15,6 +15,36 @@ class MessagesRepository(BaseRepository):
             )
         """
         await self.execute(create_table_query)
+        await self.insert(
+            id=1,
+            key="start_user",
+            name="Стартовое сообщение пользователя",
+            text="Вас приветствует официальный бот Переправы Салехард - Лабытнанги. Выберите необходимый пункт \"Меню\" или отправьте  команду /menu если меню не отобразилось",
+        )
+        await self.insert(
+            id=2,
+            key="start_moderator",
+            name="Стартовое сообщение модератора",
+            text="Вас приветствует официальный бот Переправы Салехард - Лабытнанги. Выберите необходимый пункт \"Меню\" или отправьте  команду /menu всли меню не отобразилось:",
+        )
+        await self.insert(
+            id=3,
+            key="message_types",
+            name="Тип сообщения",
+            text="Выберите тип сообщения:",
+        )
+        await self.insert(
+            id=4,
+            key="appeal_message",
+            name="Обращение ввод",
+            text="Введите текст вашего обращения.", 
+        )
+        await self.insert(
+            id=5,
+            key="appeal_message_success",
+            name="Обращение принято",
+            text="Ваше обращение принято.",
+        )
 
     async def create_message(self, message: Message):
         await self.insert(
